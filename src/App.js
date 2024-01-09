e19e43ad19a9f45634851529caaac2e3223e1947
00charan00 <poycharan@gmail.com>
2024-01-09 19:33:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <label>
+        Button:
+        <input type="button" value="Click me" />
+      </label>
+      <label>
+        Checkbox:
+        <input type="checkbox" />
+      </label>
+      <label>
+        Color:
+        <input type="color" />
+      </label>
+      <label>
+        Date:
+        <input type="date" />
+      </label>
+      <label>
+        DateTime Local:
+        <input type="datetime-local" />
+      </label>
+      <label>
+        Email:
+        <input type="email" />
+      </label>
+      <label>
+        File:
+        <input type="file" />
+      </label>
+      <label>
+        Hidden:
+        <input type="hidden" />
+      </label>
+      <label>
+        Image:
+        <input type="image" src="https://via.placeholder.com/20" alt="placeholder" />
+      </label>
+      <label>
+        Month:
+        <input type="month" />
+      </label>
+      <label>
+        Number:
+        <input type="number" />
+      </label>
+      <label>
+        Password:
+        <input type="password" />
+      </label>
+      <label>
+        Radio:
+        <input type="radio" />
+      </label>
+      <label>
+        Range:
+        <input type="range" />
+      </label>
+      <label>
+        Reset:
+        <input type="reset" />
+      </label>
+      <label>
+        Search:
+        <input type="search" />
+      </label>
+      <label>
+        Submit:
+        <input type="submit" />
+      </label>
+      <label>
+        Tel:
+        <input type="tel" />
+      </label>
+      <label>
+        Text:
+        <input type="text" defaultValue="Default Text" />
+      </label>
+      <label>
+        Time:
+        <input type="time" />
+      </label>
+      <label>
+        URL:
+        <input type="url" />
+      </label>
+      <label>
+        Week:
+        <input type="week" />
+      </label>
+      <label>
+        Dropdown Menu:
+        <select>
+          <option value="dropdownmenu">Dropdown Menu</option>
+        </select>
+      </label>
+      <label>
+        Dropdown List:
+        <select>
+          <option value="dropdownlist">Dropdown List</option>
+        </select>
+      </label>
+    </div>
+  );
+}
+
+export default App;
